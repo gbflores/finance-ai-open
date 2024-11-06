1b836d02cf6253fada072af7ecd3f24dfd065883
gbflores <guilhermebflores@gmail.com>
2024-11-06 00:59:29 -0300

    chore: add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <h1 className="p-5 text-red-500">Home</h1>
+      <h1 className="text-white-500 p-5">
+        <Button>Hello World</Button>
+      </h1>
     </div>
   );
 };
