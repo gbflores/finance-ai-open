953f44dc82fa62c7939e88bc4723232dfea995b1
gbflores <guilhermebflores@gmail.com>
2024-11-07 18:43:56 -0300

    feat: add component badge

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { Badge } from "@/app/_components/ui/badge";
-import { Transaction, TransactionType } from "@prisma/client";
+import { Transaction } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import { CircleIcon } from "lucide-react";
+import TransactionTypeBadge from "../_components/type-badge";
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -14,28 +13,7 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
     accessorKey: "type",
     header: "Tipo",
     cell: ({ row: { original: transaction } }) => {
-      if (transaction.type == TransactionType.DEPOSIT) {
-        return (
-          <Badge className="bg-muted font-bold text-primary hover:bg-muted">
-            <CircleIcon className="mr-2 fill-primary" size={10} />
-            Depósito
-          </Badge>
-        );
-      }
-      if (transaction.type == TransactionType.EXPENSE) {
-        return (
-          <Badge className="hover:bg-danger,bg-opacity-10 bg-danger bg-opacity-10 font-bold text-danger">
-            <CircleIcon className="mr-2 fill-danger" size={10} />
-            Depósito
-          </Badge>
-        );
-      }
-      return (
-        <Badge className="hover:bg-white,text-white bg-white bg-opacity-10 font-bold text-white">
-          <CircleIcon className="mr-2 fill-white" size={10} />
-          Investimento
-        </Badge>
-      );
+      <TransactionTypeBadge transaction={transaction} />;
     },
   },
   {
---
+++ app/transactions/_components/type-badge.tsx
@@ -0,0 +1,33 @@
+import { Badge } from "@/app/_components/ui/badge";
+import { Transaction, TransactionType } from "@prisma/client";
+import { CircleIcon } from "lucide-react";
+
+interface TransactionTypeBadgeProps {
+  transaction: Transaction;
+}
+const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
+  if (transaction.type == TransactionType.DEPOSIT) {
+    return (
+      <Badge className="bg-muted font-bold text-primary hover:bg-muted">
+        <CircleIcon className="mr-2 fill-primary" size={10} />
+        Depósito
+      </Badge>
+    );
+  }
+  if (transaction.type == TransactionType.EXPENSE) {
+    return (
+      <Badge className="hover:bg-danger,bg-opacity-10 bg-danger bg-opacity-10 font-bold text-danger">
+        <CircleIcon className="mr-2 fill-danger" size={10} />
+        Depósito
+      </Badge>
+    );
+  }
+  return (
+    <Badge className="hover:bg-white,text-white bg-white bg-opacity-10 font-bold text-white">
+      <CircleIcon className="mr-2 fill-white" size={10} />
+      Investimento
+    </Badge>
+  );
+};
+
+export default TransactionTypeBadge;
