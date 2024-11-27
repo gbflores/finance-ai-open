f349e8cec93fc0e0fdef8d8bb692a9f4767860ef
Guilherme <guilhermebflores@gmail.com>
2024-11-27 10:13:34 -0300

    feat: ensure user cannot create transaction on free plan and can delete a transaction

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -5,6 +5,8 @@ import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { ScrollArea } from "../_components/ui/scroll-area";
+import { canUserAddTransaction } from "../_data/can-user-add-transaction";
 
 const TransactionsPage = async () => {
   const { userId } = await auth();
@@ -15,17 +17,25 @@ const TransactionsPage = async () => {
     where: {
       userId,
     },
+    orderBy: {
+      date: "desc",
+    },
   });
+  const userCanAddTransaction = await canUserAddTransaction();
   return (
     <>
       <Navbar />
-      <div className="space-y-6 p-6">
-        {/* TÍTULO E BOTÃO */}
+      <div className="flex flex-col space-y-6 overflow-hidden p-6">
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
-          <AddTransactionButton />
+          <AddTransactionButton userCanAddTransaction={userCanAddTransaction} />
         </div>
-        <DataTable columns={transactionColumns} data={transactions} />
+        <ScrollArea className="h-full">
+          <DataTable
+            columns={transactionColumns}
+            data={JSON.parse(JSON.stringify(transactions))}
+          />
+        </ScrollArea>
       </div>
     </>
   );
