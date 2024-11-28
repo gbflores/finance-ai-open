80c7892e1ce711a122a956133985f01eb5fb20ff
Guilherme <guilhermebflores@gmail.com>
2024-11-28 15:35:54 -0300

    feat: add the toast when delete a transaction

--- app/transactions/_components/delete-transaction-button.tsx
+++ app/transactions/_components/delete-transaction-button.tsx
@@ -12,7 +12,7 @@ import {
 import { Button } from "@/app/_components/ui/button";
 import { TrashIcon } from "lucide-react";
 import { deleteTransaction } from "../_actions/delete-transaction";
-// import { toast } from "sonner";
+import { toast } from "sonner";
 
 interface DeleteTransactionButtonProps {
   transactionId: string;
@@ -24,10 +24,10 @@ const DeleteTransactionButton = ({
   const handleConfirmDeleteClick = async () => {
     try {
       await deleteTransaction({ transactionId });
-      //   toast.success("Transação deletada com sucesso!");
+      toast.success("Transação deletada com sucesso!");
     } catch (error) {
       console.error(error);
-      //   toast.error("Ocorreu um erro ao deletar a transação.");
+      toast.error("Ocorreu um erro ao deletar a transação.");
     }
   };
   return (
